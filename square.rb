fe37a84739307c76a3167b70379ce2ea8ad6446b
Trung Lê <joneslee85@gmail.com>
2013-07-23 22:37:49 +1000

    space indentation

--- square.rb
+++ square.rb
@@ -1,8 +1,8 @@
 require './rectangle.rb'
 
 class Square < Rectangle
-	def initialize(x)
-		super(x, x)		# call the method in parent class
-	end
+  def initialize(x)
+    super(x, x)	# call the method in parent class
+  end
 end
 
