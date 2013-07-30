fce4ce7518a39505684bec71ca087c37f290f1da
Erik Michaels-Ober <sferik@gmail.com>
2013-07-30 14:07:49 +0200

    Show how to pass a block to an enumerable method

--- enumerable.rb
+++ enumerable.rb
@@ -18,3 +18,7 @@ people.to_a             #=> ["Maja", "Nina", "Erik"]
 people.sort             #=> ["Erik", "Maja", "Nina"]
 people.include?("Erik") #=> true
 people.include?("Bob")  #=> false
+
+people.collect{|name| name + "is awesome!"} #=> ["Maja is awesome!", "Nina is awesome!", "Erik is awesome!"]
+people.select{|name| name.end_with?("a")}   #=> ["Maja", "Nina"]
+people.reject{|name| name.start_with?("E")} #=> ["Maja", "Nina"]
