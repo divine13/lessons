0425181f31d8d51439ac20e9af11b6122c45f79f
Erik Michaels-Ober <sferik@gmail.com>
2013-08-09 14:33:22 +0200

    Add more specs

--- ip_validator_spec.rb
+++ ip_validator_spec.rb
@@ -9,6 +9,22 @@ describe IPValidator::Validator do
       expect(validator).to be_valid
     end
 
+    it "returns false when IP address is only three digits" do
+      validator = IPValidator::Validator.new("127.0.0")
+      expect(validator).not_to be_valid
+    end
+
+    it "returns false if any number is bigger than 255" do
+      validator = IPValidator::Validator.new("256.0.0.1")
+      expect(validator).not_to be_valid
+      validator = IPValidator::Validator.new("127.256.0.1")
+      expect(validator).not_to be_valid
+      validator = IPValidator::Validator.new("12.0.256.1")
+      expect(validator).not_to be_valid
+      validator = IPValidator::Validator.new("127.0.0.256")
+      expect(validator).not_to be_valid
+    end
+
   end
 
 end
